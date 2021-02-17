28793d78da2eefef682bbd20dc6de366bc9175f7
Kole Gasior <jkole822@gmail.com>
2021-02-17 17:54:22 -0500

    use path in html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,13 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.sendFile("index.html");
-});
-
 router.get("/stats", (req, res) => {
-	res.sendFile("stats.html");
+	res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 router.get("/exercise", (req, res) => {
-	res.sendFile("exercise.html");
+	res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 module.exports = router;
