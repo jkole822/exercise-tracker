9846fa1a717807c5bf4a470c60d7e93e7a919579
Kole Gasior <jkole822@gmail.com>
2021-02-17 21:43:43 -0500

    create api-routes

---
+++ routes/api-routes.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+
+const Workout = mongoose.model("workout");
+
+router.get("/api/workouts", async (req, res) => {
+	try {
+		const workouts = await Workout.aggregate([
+			{
+				$addFields: {
+					totalDuration: {
+						$sum: "$exercises.duration",
+					},
+				},
+			},
+		]).exec();
+
+		res.json(workouts);
+	} catch (e) {
+		res.status(500).send("Error: ", e.message);
+	}
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+	try {
+		const workouts = await Workout.aggregate([
+			{
+				$addFields: {
+					totalDuration: {
+						$sum: "$exercises.duration",
+					},
+				},
+			},
+		]).exec();
+
+		res.json(workouts);
+	} catch (e) {
+		res.status(500).send("Error: ", e.message);
+	}
+});
+
+router.post("/api/workouts", async (req, res) => {
+	try {
+		const workout = await Workout.create(req.body);
+
+		res.json(workout);
+	} catch (e) {
+		res.status(500).send("Error: ", e.message);
+	}
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+	try {
+		const id = req.params.id;
+
+		const workout = await Workout.findByIdAndUpdate(id, {
+			$push: { exercises: req.body },
+		});
+
+		res.json(workout);
+	} catch (e) {
+		res.status(500).send("Error: ", e.message);
+	}
+});
+
+module.exports = router;
