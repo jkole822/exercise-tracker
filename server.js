32865f4892ce088c2fd8f078ab021bf3483272b3
Kole Gasior <jkole822@gmail.com>
2021-02-17 21:45:06 -0500

    require in workout model and api-routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require("./models/workout");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const htmlRoutes = require("./routes/html-routes");
+const apiRoutes = require("./routes/api-routes");
 const keys = require("./config/keys.js");
-
 const app = express();
 
 mongoose.connect(keys.mongoURI, {
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("combined"));
 app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
